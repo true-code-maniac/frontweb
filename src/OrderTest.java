53690689b01d231e889a135b170b8f71cd721851
Bojken Sina <sinab.ficufish@gmail.com>
2022-03-05 11:54:14 +0100

    The buttons for the application

--- src/OrderTest.java
+++ src/OrderTest.java
@@ -1,6 +1,22 @@
+import javax.swing.*;
 
 public class OrderTest {
     public static void main(String []args){
-        System.out.println("This is a message from the application and it is working");
+        JFrame f=new JFrame();
+
+        JButton checkButton= new JButton("Check");
+        checkButton.setBounds(50,900,100, 25);
+        JButton addButton = new JButton("Add");
+        addButton.setBounds(250, 900, 100, 25);
+        JButton showButton = new JButton("Show");
+        showButton.setBounds(450, 900, 100, 25);
+
+        f.setTitle("OrderTest/Forward");
+        f.add(checkButton);
+        f.add(addButton);
+        f.add(showButton);
+        f.setSize(1000,1000);
+        f.setLayout(null);
+        f.setVisible(true);
     }
 }
