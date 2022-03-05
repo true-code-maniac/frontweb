73caeb8b47b011efd026d8cf10e6efe7096f45b7
Bojken Sina <sinab.ficufish@gmail.com>
2022-03-05 11:33:36 +0100

    renaming class

--- src/OrderTest.java
+++ src/OrderTest.java
@@ -1,4 +1,5 @@
-public class Application {
+
+public class OrderTest {
     public static void main(String []args){
         System.out.println("This is a message from the application and it is working");
     }
