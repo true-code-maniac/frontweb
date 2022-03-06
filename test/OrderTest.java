ae4674dc4171705266de3f06f77d0a9a47d07d0e
raven <raven@MacBook-Pro.local>
2022-03-06 10:03:47 +0100

    Correct test for get total cost

--- test/OrderTest.java
+++ test/OrderTest.java
@@ -1,42 +1,25 @@
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import org.junit.*;
+import java.util.*;
 
 public class OrderTest {
 
-    private final Product productOne = new Product("Jacket", "Clothing", "300", "Shipping", 2020,300.0);
-    private final Product productTwo = new Product("Blue Jacket", "Clothing", "300", "Shipping", 2020,150.0);
-    private final Product productThree = new Product("Red Jacket", "Clothing", "300", "Shipping", 2020,50.0);
-    private final Product productFour = new Product("Black Jacket", "Clothing", "300", "In Magazine", 2020, 0.0);
-
-    /**
-     * @Test getStorageSizeOne
-     * @description The first test is  a very simple test in  order to check if
-     * if the specified product quantities are added in storage. If test passes then it is
-     * a success otherwise it is a failure.
-     */
-
     @Test
-    public void getStorageSizeOne(){
-        Warehouse warehouse = new Warehouse();
-        warehouse.addProductQuantity(productOne.getDescription(), productOne, 30);
-        warehouse.addProductQuantity(productTwo.getDescription(), productTwo, 20);
-        warehouse.addProductQuantity(productThree.getDescription(), productTwo, 10);
-        assertTrue(warehouse.getStorage().keySet().size() > 0);
-        assertEquals(warehouse.getStorage().keySet().size(), 3);
-    }
+    public void getTotalCostTest(){
+        Order order = new Order();
+        Product productOne = new Product("Jacket", "Clothing", "300", "Shipping", 2020,300.0);
+        Product productTwo = new Product("Blue Jacket", "Clothing", "300", "Shipping", 2020,150.0);
+        Product productThree = new Product("Red Jacket", "Clothing", "300", "Shipping", 2020,50.0);
+        Product productFour = new Product("Black Jacket", "Clothing", "300", "In Magazine", 2020, 0.0);
 
-    @Test
-    public void getStorageSizeTwo(){
-        Warehouse warehouse = new Warehouse();
-        warehouse.addProductQuantity(productOne.getDescription(), productOne, 30);
-        warehouse.addProductQuantity(productOne.getDescription(), productOne, 30);
-        warehouse.addProductQuantity(productFour.getDescription(), productFour,  10);
-        warehouse.addProductQuantity(productFour.getDescription(), productFour,10);
-        assertTrue(warehouse.getStorage().keySet().size() > 0);
-        assertEquals(warehouse.getStorage().keySet().size(), 2);
-        assertEquals(warehouse.getStorage().get(productOne.getDescription()).getQuantity(), 60);
-        assertEquals(warehouse.getStorage().get(productFour.getDescription()).getQuantity(), 20);
+        ProductQuantity productQuantityOne = new ProductQuantity(productOne, 2);
+        ProductQuantity productQuantityTwo = new ProductQuantity(productTwo,3);
+        ProductQuantity productQuantityThree = new ProductQuantity(productThree, 2);
+        ProductQuantity productQuantityFour = new ProductQuantity(productFour, 3);
+
+        List<ProductQuantity> listOfProducts = List.of(productQuantityOne, productQuantityTwo, productQuantityThree, productQuantityFour);
+        order.setProductQuantities(listOfProducts);
+
+        assertEquals(order.getCost(), 1150.0, 0.0);
     }
 }
---
+++ test/WarehouseTest.java
@@ -0,0 +1,42 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class WarehouseTest {
+
+    private final Product productOne = new Product("Jacket", "Clothing", "300", "Shipping", 2020,300.0);
+    private final Product productTwo = new Product("Blue Jacket", "Clothing", "300", "Shipping", 2020,150.0);
+    private final Product productThree = new Product("Red Jacket", "Clothing", "300", "Shipping", 2020,50.0);
+    private final Product productFour = new Product("Black Jacket", "Clothing", "300", "In Magazine", 2020, 0.0);
+
+    /**
+     * @Test getStorageSizeOne
+     * @description The first test is  a very simple test in  order to check if
+     * if the specified product quantities are added in storage. If test passes then it is
+     * a success otherwise it is a failure.
+     */
+
+    @Test
+    public void getStorageSizeOne(){
+        Warehouse warehouse = new Warehouse();
+        warehouse.addProductQuantity(productOne.getDescription(), productOne, 30);
+        warehouse.addProductQuantity(productTwo.getDescription(), productTwo, 20);
+        warehouse.addProductQuantity(productThree.getDescription(), productTwo, 10);
+        assertTrue(warehouse.getStorage().keySet().size() > 0);
+        assertEquals(warehouse.getStorage().keySet().size(), 3);
+    }
+
+    @Test
+    public void getStorageSizeTwo(){
+        Warehouse warehouse = new Warehouse();
+        warehouse.addProductQuantity(productOne.getDescription(), productOne, 30);
+        warehouse.addProductQuantity(productOne.getDescription(), productOne, 30);
+        warehouse.addProductQuantity(productFour.getDescription(), productFour,  10);
+        warehouse.addProductQuantity(productFour.getDescription(), productFour,10);
+        assertTrue(warehouse.getStorage().keySet().size() > 0);
+        assertEquals(warehouse.getStorage().keySet().size(), 2);
+        assertEquals(warehouse.getStorage().get(productOne.getDescription()).getQuantity(), 60);
+        assertEquals(warehouse.getStorage().get(productFour.getDescription()).getQuantity(), 20);
+    }
+
+
+}
